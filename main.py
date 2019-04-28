51f6b4f8f77b2c765245995f4521fcf907c1d665
ahmed.nasr9677@gmail.com <ahmed.nasr9677@gmail.com>
2019-04-28 21:20:19 +0200

    adding the main window code file

---
+++ main.py
@@ -0,0 +1,73 @@
+from PyQt5.uic import loadUiType
+import sys
+from os import path
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMenu, QVBoxLayout, QSizePolicy, QMessageBox, QWidget, \
+    QPushButton, QCheckBox, QLabel, QLineEdit, QComboBox
+from PyQt5.QtGui import QIcon
+
+
+
+FORM_CLASS, _ = loadUiType(path.join(path.dirname(__file__), "Memory.ui"))
+
+
+class MainApp(QMainWindow, FORM_CLASS):
+
+    def __init__(self, parent= None):
+        super(MainApp, self).__init__(parent)
+        QMainWindow.__init__(self)
+        self.setupUi(self)
+        self.setup_Ui()
+        self.init_Buttons()
+    
+        
+
+
+        
+
+
+    def setup_Ui(self):
+        '''
+        UI setup goes here
+        '''
+        self.center_window()
+        self.setWindowTitle("Memory Management")
+
+        
+        
+
+    def center_window(self):
+        # centering window
+        qtRectangle = self.frameGeometry()
+        centerPoint = QDesktopWidget().availableGeometry().center()
+        qtRectangle.moveCenter(centerPoint)
+        self.move(qtRectangle.topLeft())
+        
+
+
+    def init_Buttons(self):
+        '''
+        Buttons initializations and slots connections goes here
+        '''
+        pass
+
+
+
+        
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainApp()
+    window.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
+
+
