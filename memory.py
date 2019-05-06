c52532c33282a841db081e7330f8dc636df3a90e
MohamedAliRashad <m.rashadnow@gmail.com>
2019-05-06 21:39:13 +0200

    Adding add_hole method

--- memory.py
+++ memory.py
@@ -4,16 +4,16 @@ from operator import itemgetter
 
 class Memory():
 
-    memory_contents = []
+    def __init__(self, memory_size):
+        
+        self.memory_contents = [["default", self.color_from_name("default"), memory_size]]
+        self.memory_size = memory_size
 
-
-    def __init__(self):
-        pass
         
     def color_from_name(self, process_name="hole"):
         return hashlib.md5(process_name.encode()).hexdigest()[0:6]
 
-    def first_fit(segments_list , hole_list ,memory_size):
+    def first_fit(self, segments_list , hole_list ,memory_size):
 
         # starting address,name,size
         temp2_list=[]
@@ -114,7 +114,7 @@ class Memory():
 
     def worst_fit(self, Memory, Segments, process_name="default"):
         
-        color = color_from_name(process_name)
+        color = self.color_from_name(process_name)
         error = 0
 
         for segment in Segments:
@@ -140,10 +140,29 @@ class Memory():
     def compact(self):
         pass
     
-    def add_hole(self):
-        pass
+    def add_hole(self, starting_address, hole_size):
+        assert(starting_address + hole_size <= self.memory_size)
+        sum = 0
+        for i in range(len(self.memory_contents)):
+            sum = sum + self.memory_contents[i][2]
+            if(sum > starting_address):
+                assert(self.memory_contents[i][0] == "default")
+                assert(sum - (starting_address + hole_size) >= 0)
+                if sum - (starting_address + hole_size) == 0:
+                    self.memory_contents[i][2] -= hole_size
+                    self.memory_contents.insert(i+1, ["hole", self.color_from_name("hole"), hole_size])
+                    break
+                else:
+                    self.memory_contents[i][2] = self.memory_contents[i][2] - (sum - starting_address)
+                    self.memory_contents.insert(i+1, ["hole", self.color_from_name("hole"), hole_size])
+                    self.memory_contents.insert(i+2, ["default", self.color_from_name("default"), sum - (starting_address + hole_size)])
+                    if (self.memory_contents[i][2] == 0):
+                        self.memory_contents.pop(i)
+                    break
+                    
+        # self.memory_contents.insert[]
     
-    def add_process(self, process):
+    def deallocate(self, process_name):
         pass
 
 
@@ -154,13 +173,12 @@ class Memory():
         return self.memory_contents
 
 
-    '''
-    Memory = [["hole", "ca3143", 100]]
-    Segments = [["Code", 5],
-                ["Data", 90],
-                ["Stack", 15]]
+Segments = [["Code", 5],
+            ["Data", 90],
+            ["Stack", 15]]
 
-    Memory, _ = worst_fit(Memory, Segments, "p1")
-    print(Memory, _)
-
-    '''
+memory = Memory(5000)
+memory.add_hole(2000, 3000)
+print(memory.get_memoryContents())
+# Memory, _ = worst_fit(Memory, Segments, "p1")
+# print(Memory, _)
