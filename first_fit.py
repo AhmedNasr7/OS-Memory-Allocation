2e333313d0b8e4790f98d9a6a6ca127dc2046a90
MohamedAliRashad <m.rashadnow@gmail.com>
2019-05-07 23:46:57 +0200

    Minor Changes

--- first_fit.py
+++
@@ -1,8 +0,0 @@
-def firt_fit (slef ,segment_list ,hole_list,memory_size): 
-    for n in range (len(segments_list)):
-            temp_hole_list=copy.deepcopy(hole_list)
-            segements=0
-            for j in range (len(segements_list[n])):
-
-             for k in range (len(temp_hole_list)):
-                 if(segements_l)
--- memory.py
+++ memory.py
@@ -19,12 +19,12 @@ class Memory():
         color=self.color_from_name(process_name)                                                                                                                                                                                                                                                                                                                                                                                            
         segements=0
         
-        for i in range (len(segements_list)):
+        for i in range (len(segments_list)):
             for j in range (len(output_list)):
                 if(output_list[j][0]=='hole'):
-                    if(output_list[j][2]>=segements_list[i][1]):
-                       output_list[j][2]= output_list[j][2]-segements_list[i][1]
-                       output_list.insert(j,[segements_list[i][0],color,segements_list[i][1]])
+                    if(output_list[j][2]>=segments_list[i][1]):
+                       output_list[j][2]= output_list[j][2]-segments_list[i][1]
+                       output_list.insert(j,[segments_list[i][0],color,segments_list[i][1]])
                        if(output_list[j+1][2]==0):
                            output_list.pop(j+1)
                        segements=segements+1
@@ -32,13 +32,9 @@ class Memory():
             
                     
        
-            if(segements==len(segements_list)):
+            if(segements==len(segments_list)):
              self.memory_contents=copy.deepcopy(output_list)
       
-
-
-
-
     def best_fit(self, segments, process_name):
         min_size = self.memory_contents[0][2]
         hole_index = 0
@@ -63,25 +59,7 @@ class Memory():
             else:
                 self.memory_contents[hole_index] = [process_name, self.color_from_name(process_name), segment_size]
 
-                
-            
-            
-
-                        
-            
-
-                
-
-                
-
-            
-        
-
-        
-        
-
-    def worst_fit(self, Segments, process_name="default"):
-        
+    def worst_fit(self, Segments, process_name="default"):      
         color = self.color_from_name(process_name)
 
         for segment in Segments:
@@ -89,7 +67,8 @@ class Memory():
             for i in range(len(self.memory_contents)):
                 if(self.memory_contents[i][2] > self.memory_contents[Max][2] and self.memory_contents[i][0] == "hole"):
                     Max = i
-                    assert(self.memory_contents[Max][2] >= segment[1]) # i modified this assersion place, is it right?
+        
+            assert(self.memory_contents[Max][2] >= segment[1]), "Memory limit Excedded"
 
             self.memory_contents.insert(Max, [segment[0], color, segment[1]])
 
@@ -98,25 +77,33 @@ class Memory():
             else:
                 self.memory_contents[Max+1][2] = self.memory_contents[Max+1][2] - segment[1]
             
-
     def compact(self):
         holes_sum = 0
         for hole in self.memory_contents[:]:
             if(hole[0] == "hole"):
                 holes_sum = holes_sum + hole[2]
                 self.memory_contents.remove(hole)
-            
+        
         self.memory_contents.insert(0, ["hole", self.color_from_name(), holes_sum])
+        
+        
+    # def Merge(self):
+    #     memory_copy = []
+    #     for i in range(len(self.memory_contents) - 1):
+    #         if(self.memory_contents[i][0] == self.memory_contents[i+1][0]):
+    #             self.memory_contents[i][2] = self.memory_contents[i][2] + self.memory_contents[i+1][2]
+                
+    #             i = i + 1
+                
     
-
     def add_hole(self, starting_address, hole_size):
         assert(starting_address + hole_size <= self.memory_size)
         sum = 0
         for i in range(len(self.memory_contents)):
             sum = sum + self.memory_contents[i][2]
             if(sum > starting_address):
-                assert(self.memory_contents[i][0] == "default")
-                assert(sum - (starting_address + hole_size) >= 0)
+                assert(self.memory_contents[i][0] == "default"), "Can't add a hole here"
+                assert(sum - (starting_address + hole_size) >= 0), "Hole size excedded the limits"
                 if sum - (starting_address + hole_size) == 0:
                     self.memory_contents[i][2] -= hole_size
                     self.memory_contents.insert(i+1, ["hole", self.color_from_name("hole"), hole_size])
@@ -129,7 +116,6 @@ class Memory():
                         self.memory_contents.pop(i)
                     break
                     
-        # self.memory_contents.insert[]
     
     def deallocate(self, process_name):
         color = self.color_from_name(process_name)
@@ -145,9 +131,9 @@ class Memory():
         return self.memory_contents
 
 
-'''
+
 # # Testing
-Segments = [["Code", 5000],
+Segments = [["Code", 50],
             ["Data", 90],
            ["Stack", 15]]
 
@@ -159,11 +145,18 @@ memory.add_hole(0,700)
 
 memory.add_hole(1000, 1000)
 
-print(memory.get_memoryContents())
-memory.worst_fit(Segments, "P1")
-print(memory.get_memoryContents())
-memory.deallocate("P1")
+memory.add_hole(3000, 800)
+
 print(memory.get_memoryContents())
 memory.compact()
 print(memory.get_memoryContents())
-'''
+# memory.Merge()
+print(memory.get_memoryContents())
+# memory.worst_fit(Segments, "P1")
+# print(memory.get_memoryContents())
+# memory.deallocate("P1")
+# print(memory.get_memoryContents())
+# memory.compact()
+# print(memory.get_memoryContents())
+# memory.Merge()
+# print(memory.get_memoryContents())
