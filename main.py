1ab172aab594a8c66269f6607f37ec35b1c97d27
MohamedAliRashad <m.rashadnow@gmail.com>
2019-05-13 01:10:08 +0200

    Adding the Exutable file

--- main.py
+++ main.py
@@ -14,12 +14,13 @@ from PyQt5.QtWidgets import (QApplication, QCheckBox, QComboBox,
 from PyQt5.uic import loadUiType
 
 from memory import Memory
-from segments import *
+# from segments import *
 
-FORM_CLASS, _ = loadUiType(path.join(path.dirname(__file__), "MemoryV2.ui"))
+Ui_MainWindow, _ = loadUiType(path.join(path.dirname(__file__), "/home/rashad/Work/GitHub_Projects/OS-Memory-Allocation/MemoryV2.ui"))
+Ui_Segments,_ = loadUiType(path.join(path.dirname(__file__), "/home/rashad/Work/GitHub_Projects/OS-Memory-Allocation/segments.ui"))
 
 
-class MainApp(QMainWindow, FORM_CLASS):
+class MainApp(QMainWindow, Ui_MainWindow):
 
     process_Num = 0
     segments_list = []
@@ -265,6 +266,100 @@ class MainApp(QMainWindow, FORM_CLASS):
         self.msgBox.setStandardButtons(QMessageBox.Ok)
         self.msgBox.exec_()
 
+
+class SegmentWindow(QMainWindow, Ui_Segments):
+
+
+    segmentsData_passingSig = pyqtSignal('QVariantList')
+
+    def __init__(self, parent= None):
+        super(SegmentWindow, self).__init__(parent)
+        QMainWindow.__init__(self)
+        self.setupUi(self)
+        self.columns_count = 2
+        self.rows_count = 0 
+        self.table_width = 430
+        self.table_height = 590
+        self.segments_list = [] # the list represents the memory contents of segments.
+        self.setup_Ui()
+        self.init_Buttons()
+        #self.process_Num += 1
+        #self.processNo_label.setText('Process:   P' + str(self.process_Num))
+
+
+    def setup_Ui(self):
+        self.setWindowTitle("Memory Management")
+    
+
+    def setup_table(self):
+     
+        self.table.setRowCount(self.rows_count)
+        self.table.setColumnCount(self.columns_count)
+        columnsLabels = ['Segment Name', 'Size']
+        self.table.setHorizontalHeaderLabels(columnsLabels)
+        self.table.setFixedSize(self.table_width, self.table_height)
+        for i in range(self.columns_count):
+            self.table.setColumnWidth(i, self.table_width/2)
+
+
+
+    def init_Buttons(self):
+        
+        self.cancel_button.clicked.connect(self.cancel)
+        self.clear_button.clicked.connect(self.clear)
+        self.addSegments_button.clicked.connect(self.add_segments)
+
+
+    @pyqtSlot()
+    def set_segmentsNo(self, segmentsNo):
+        self.rows_count = segmentsNo
+        self.setup_table()
+
+
+    def add_segments(self):
+        for i in range(self.rows_count):
+            try:
+                name = self.table.takeItem(i, 0).text()
+                size =  int(self.table.takeItem(i, 1).text())
+                segment = [name, size] # color is to be generated here
+                self.segments_list.append(segment)
+            except ValueError as e:
+                self.show_msgBox("Input Value Error!\nSize must be numerical value.") # a code should be added to handle the case with no enough data.
+            
+            # code to handle sizes that exceed memory limit?
+        
+        if len(self.segments_list) > 0:
+            self.segmentsData_passingSig.emit(self.segments_list)
+            self.processes_Num += 1
+        
+        else:
+            self.show_msgBox("Input Error!\nYou can't leave segment data table empty!") # a code should be added to handle the case with no enough data.
+    
+
+    def set_processesNum(self, num):
+        self.processes_Num = num
+        self.processNo_label.setText('Process:   P' + str(num))
+      
+
+    def clear(self):
+          for i in range (self.rows_count):
+            self.table.setItem(i, 0, QTableWidgetItem(''))
+            self.table.setItem(i, 1, QTableWidgetItem(''))
+            self.table.setItem(i, 2, QTableWidgetItem(''))
+
+
+    def cancel(self):
+        self.close()
+
+
+    def show_msgBox(self, msg):
+        self.msgBox = QMessageBox()
+        self.msgBox.setWindowTitle("Error!")
+        self.msgBox.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        self.msgBox.setIcon(QMessageBox.Warning)
+        self.msgBox.setText(msg)
+        self.msgBox.setStandardButtons(QMessageBox.Ok)
+        self.msgBox.exec_()
 
 def hex_to_qcolor(value):
     value = value.lstrip("#")
--- memory.py
+++ memory.py
@@ -1,39 +1,43 @@
-import hashlib 
+import hashlib
 import copy
 from operator import itemgetter
 
-class Memory():
 
+class Memory:
     def __init__(self, memory_size):
-        
-        self.memory_contents = [["old_process", self.color_from_name("old_process"), memory_size]]
+
+        self.memory_contents = [
+            ["old_process", self.color_from_name("old_process"), memory_size]
+        ]
         self.memory_size = memory_size
 
     @staticmethod
     def color_from_name(process_name="hole"):
         return hashlib.md5(process_name.encode()).hexdigest()[0:6]
 
-
     def first_fit(self, segments_list, process_name):
-        #name,color,size
-        output_list=copy.deepcopy(self.memory_contents)
-        color=self.color_from_name(process_name)                                                                                                                                                                                                                                                                                                                                                                                            
-        segements=0
-        
-        for i in range (len(segments_list)):
-            for j in range (len(output_list)):
-                if(output_list[j][0]=='hole'):
-                    if(output_list[j][2]>=segments_list[i][1]):
-                       output_list[j][2]= output_list[j][2]-segments_list[i][1]
-                       output_list.insert(j,[segments_list[i][0],color,segments_list[i][1]])
-                       if(output_list[j+1][2]==0):
-                           output_list.pop(j+1)
-                       segements=segements+1
-                       break            
-        assert(segements==len(segments_list)),"Not Enough memory to allocate this process"
-        self.memory_contents=copy.deepcopy(output_list)
-            
-      
+        # name,color,size
+        output_list = copy.deepcopy(self.memory_contents)
+        color = self.color_from_name(process_name)
+        segements = 0
+
+        for i in range(len(segments_list)):
+            for j in range(len(output_list)):
+                if output_list[j][0] == "hole":
+                    if output_list[j][2] >= segments_list[i][1]:
+                        output_list[j][2] = output_list[j][2] - segments_list[i][1]
+                        output_list.insert(
+                            j, [segments_list[i][0], color, segments_list[i][1]]
+                        )
+                        if output_list[j + 1][2] == 0:
+                            output_list.pop(j + 1)
+                        segements = segements + 1
+                        break
+        assert segements == len(
+            segments_list
+        ), "Not Enough memory to allocate this process"
+        self.memory_contents = copy.deepcopy(output_list)
+
     def best_fit(self, segments, process_name):
 
         # segments = [name, size]
@@ -45,119 +49,152 @@ class Memory():
             min_size = self.memory_size + 1
             for i in range(len(self.memory_contents)):
                 hole = self.memory_contents[i]
-                if (hole[0] == 'hole'):
+                if hole[0] == "hole":
                     hole_size = hole[2]
-                    if ((hole_size >= segment_size) and (hole_size < min_size)):
+                    if (hole_size >= segment_size) and (hole_size < min_size):
                         min_size = hole_size
                         hole_index = i
                         Flag = True
-            assert(Flag), "Not Enough memory to allocate this process"
+            assert Flag, "Not Enough memory to allocate this process"
             best_hole = self.memory_contents[hole_index]
             hole_size = best_hole[2]
             left_over = hole_size - segment_size
-            if(left_over > 0):
+            if left_over > 0:
                 best_hole[2] = left_over
                 self.memory_contents[hole_index] = best_hole
-                self.memory_contents.insert(hole_index, [segment_name, self.color_from_name(process_name), segment_size])
+                self.memory_contents.insert(
+                    hole_index,
+                    [segment_name, self.color_from_name(process_name), segment_size],
+                )
             else:
-                self.memory_contents[hole_index] = [segment_name, self.color_from_name(process_name), segment_size]
+                self.memory_contents[hole_index] = [
+                    segment_name,
+                    self.color_from_name(process_name),
+                    segment_size,
+                ]
 
-    def worst_fit(self, Segments, process_name):      
+    def worst_fit(self, Segments, process_name):
         color = self.color_from_name(process_name)
 
         for segment in Segments:
             Max = 0
             Flag = False
             for i in range(len(self.memory_contents)):
-                if(self.memory_contents[i][2] >= self.memory_contents[Max][2] and self.memory_contents[i][0] == "hole"):
+                if (
+                    self.memory_contents[i][2] >= self.memory_contents[Max][2]
+                    and self.memory_contents[i][0] == "hole"
+                ):
                     Max = i
                     Flag = True
-        
-            assert(Flag and self.memory_contents[Max][2] >= segment[1]), "Not Enough memory to allocate this process"
+
+            assert (
+                Flag and self.memory_contents[Max][2] >= segment[1]
+            ), "Not Enough memory to allocate this process"
             # print(self.memory_contents[Max][2])
             # print(segment[1])
 
             self.memory_contents.insert(Max, [segment[0], color, segment[1]])
 
-            if(self.memory_contents[Max+1][2] == segment[1]):
-                self.memory_contents.pop(Max+1)
+            if self.memory_contents[Max + 1][2] == segment[1]:
+                self.memory_contents.pop(Max + 1)
             else:
-                self.memory_contents[Max+1][2] = self.memory_contents[Max+1][2] - segment[1]
-
+                self.memory_contents[Max + 1][2] = (
+                    self.memory_contents[Max + 1][2] - segment[1]
+                )
 
     def compact(self):
         holes_sum = 0
         for hole in self.memory_contents[:]:
-            if(hole[0] == "hole"):
+            if hole[0] == "hole":
                 holes_sum = holes_sum + hole[2]
                 self.memory_contents.remove(hole)
-        
+
         self.memory_contents.insert(0, ["hole", self.color_from_name(), holes_sum])
-        
-        
+
     def Merge(self):
         Flag = 1
-        while(Flag):
+        while Flag:
             Flag = 0
             for i in range(len(self.memory_contents) - 1):
-                if(self.memory_contents[i][0] == self.memory_contents[i+1][0]):
-                    self.memory_contents[i][2] = self.memory_contents[i][2] + self.memory_contents[i+1][2]
-                    self.memory_contents.pop(i+1)
+                if self.memory_contents[i][0] == self.memory_contents[i + 1][0]:
+                    self.memory_contents[i][2] = (
+                        self.memory_contents[i][2] + self.memory_contents[i + 1][2]
+                    )
+                    self.memory_contents.pop(i + 1)
                     Flag = 1
                     break
-                
-    
+
     def add_hole(self, starting_address, hole_size):
-        assert starting_address + hole_size <= self.memory_size, "Hole Address or Size exceeds memory limits, please reassign their values"
+        assert (
+            starting_address + hole_size <= self.memory_size
+        ), "Hole Address or Size exceeds memory limits, please reassign their values"
         sum = 0
         for i in range(len(self.memory_contents)):
             sum = sum + self.memory_contents[i][2]
-            if(sum > starting_address):
-                assert(self.memory_contents[i][0] == "old_process"), "Can't add a hole here"
-                assert(sum - (starting_address + hole_size) >= 0), "Hole size excedded the limits"
+            if sum > starting_address:
+                assert (
+                    self.memory_contents[i][0] == "old_process"
+                ), "Can't add a hole here"
+                assert (
+                    sum - (starting_address + hole_size) >= 0
+                ), "Hole size excedded the limits"
                 if sum - (starting_address + hole_size) == 0:
                     self.memory_contents[i][2] -= hole_size
-                    self.memory_contents.insert(i+1, ["hole", self.color_from_name("hole"), hole_size])
+                    self.memory_contents.insert(
+                        i + 1, ["hole", self.color_from_name("hole"), hole_size]
+                    )
                     break
                 else:
-                    self.memory_contents[i][2] = self.memory_contents[i][2] - (sum - starting_address)
-                    self.memory_contents.insert(i+1, ["hole", self.color_from_name("hole"), hole_size])
-                    self.memory_contents.insert(i+2, ["old_process", self.color_from_name("old_process"), sum - (starting_address + hole_size)])
-                    if (self.memory_contents[i][2] == 0):
+                    self.memory_contents[i][2] = self.memory_contents[i][2] - (
+                        sum - starting_address
+                    )
+                    self.memory_contents.insert(
+                        i + 1, ["hole", self.color_from_name("hole"), hole_size]
+                    )
+                    self.memory_contents.insert(
+                        i + 2,
+                        [
+                            "old_process",
+                            self.color_from_name("old_process"),
+                            sum - (starting_address + hole_size),
+                        ],
+                    )
+                    if self.memory_contents[i][2] == 0:
                         self.memory_contents.pop(i)
                     break
-                    
-    
+
     def deallocate(self, process_name):
         color = self.color_from_name(process_name)
         for i in range(len(self.memory_contents)):
-            if(self.memory_contents[i][1] == color):
-                self.memory_contents[i] = ["hole", self.color_from_name(), self.memory_contents[i][2]]
-
+            if self.memory_contents[i][1] == color:
+                self.memory_contents[i] = [
+                    "hole",
+                    self.color_from_name(),
+                    self.memory_contents[i][2],
+                ]
 
     def get_memoryContents(self):
-        '''
+        """
         return list of memory processes/segments
-        '''    
+        """
         return self.memory_contents
 
     def get_memorySize(self):
-        '''
+        """
         return list of memory processes/segments
-        '''    
+        """
         return self.memory_size
+
 
 def main():
     # # Testing
-    Segments = [["Code", 50],
-                ["Data", 90],
-            ["Stack", 15]]
+    Segments = [["Code", 50], ["Data", 90], ["Stack", 15]]
 
     memory = Memory(5000)
 
     memory.add_hole(2000, 500)
 
-    memory.add_hole(0,700)
+    memory.add_hole(0, 700)
 
     memory.add_hole(1000, 1000)
 
@@ -177,7 +214,6 @@ def main():
     # memory.Merge()
     # print(memory.get_memoryContents())
 
-    
     # # previously wrong test case --> fixed
     # segements_list=[['code',100],['stack',30]]
     # Memory1=Memory(350)
@@ -190,7 +226,6 @@ def main():
     # Memory1.worst_fit(segements_list, 'abc')
     # print(Memory1.get_memoryContents())
 
-    
 
 if __name__ == "__main__":
     main()
